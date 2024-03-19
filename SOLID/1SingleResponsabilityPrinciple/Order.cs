6fe6622a023748e1f8412ece402a2cf3bbc25860
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-19 20:00:09 -0300

    :parkles:

--- SOLID/1SingleResponsabilityPrinciple/Order.cs
+++ SOLID/1SingleResponsabilityPrinciple/Order.cs
@@ -3,14 +3,25 @@ namespace _1SingleResponsabilityPrinciple;
 public class Order
 {
     public long Quantity { get; private set;}
-    
     public DateTime Data { get; set; }
-
+    private ILogger? _logger;
+    private SendEmail _sendEmail;
+    public Order()
+    {
+        _sendEmail = new SendEmail();
+        _logger = new Logger();
+    }
+    
     public void AddOrder()
     {
         try
         {
-            Quantity = 100;
+            _logger.Info("Including order");
+            _sendEmail.EmailFrom = "mabatista1@gmail.com";
+            _sendEmail.EmailTo = "mabatista2@gmail.com";
+            _sendEmail.EmailSubject = "Single Responsability Principle";
+            _sendEmail.EmailBody = "Learning the first principle of SOLID Principles";
+            _sendEmail.Send();
         }
         catch (Exception e)
         {
--- SOLID/1SingleResponsabilityPrinciple/SendEmail.cs
+++ SOLID/1SingleResponsabilityPrinciple/SendEmail.cs
@@ -2,5 +2,18 @@ namespace _1SingleResponsabilityPrinciple;
 
 public class SendEmail
 {
+    public string EmailFrom { get; set; }
+    public string EmailTo { get; set; }
+    public string EmailSubject { get; set; }
+    public string EmailBody { get; set; }
+
+    public void Send()
+    {
+        Console.WriteLine($"From: {EmailFrom}");
+        Console.WriteLine($"To: {EmailTo}");
+        Console.WriteLine($"Subject: {EmailSubject}");
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine($"{EmailBody}");
+    }
     
 }
